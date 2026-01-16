db5f646ffee98922c9c07f6e09c3c2b6465cb2e0
Syeda Aliza Hassan Taba Tabai <asifaliza61@gmail.com>
2026-01-16 16:59:09 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html"],
+  theme: {
+    extend: {
+      screens: {
+         sm:"480px",   
+            },
+    },
+  },
+  plugins: [],
+};
